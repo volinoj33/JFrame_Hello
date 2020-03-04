2794560e8fd6924a42c9ce97e5a952132b28a09e
James Volino <58526437+volinoj33@users.noreply.github.com>
2020-03-04 10:09:18 -0500

    Initial Commit

---
+++ JHelloDemo.java
@@ -0,0 +1,13 @@
+//Application for JHelloFrame.java
+//TextBook pg:728
+
+public class JHelloDemo
+{
+	public static void main(String [] args)
+	{
+		JHelloFrame frame = new JHelloFrame();
+		frame.setVisible(true);
+	}
+}
+
+//LETS GOOOO BABAY!!!
